ed418bed1616e35edc1bf89c72d780efc1b7974d
jiwon111 <dreamjiwon@naver.com>
2021-01-22 01:36:10 +0900

    node module

--- ch02_test8.js
+++ ch02_test8.js
@@ -10,4 +10,8 @@ var curPath = path.join('/Users/mike','notepad.exe');
 console.log('파일 패스 : %s', curPath);
 
 //패스에서 디렉토리, 파일명, 확장자 구분하기
-var filename
+var filename="C:\\Users\\mike\\notepad.exe";
+var dirname=path.dirname(filename);
+var basename=path.basename(filename);
+var extname=path.extname(filename);
+console.log('디렉토리 : %s, 파일 이름 : %s, 확장자 : %s',dirname,basename,extname);
