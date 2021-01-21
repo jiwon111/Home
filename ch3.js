8913a19eaa683ae9d9f04d8df7fbf5ae2de50318
jiwon111 <dreamjiwon@naver.com>
2021-01-22 01:21:19 +0900

    first

---
+++ ch3.js
@@ -0,0 +1,11 @@
+/*var result=0;
+
+console.time('duration_sum');
+for (var i =1; i<1000;i++){
+    result+=i;
+}
+console.timeEnd('duration_sum');
+console.log('1부터 1000까지 더한 결과물 : %d',result);*/
+var nconf = require('nconf');
+nconf.env();
+console.log('OS 환경 변수의 값 : %s', nconf.get('OS'));
