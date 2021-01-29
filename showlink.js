475d890c6e8dd32f202a7846339815fffae79ade
jiwon111 <dreamjiwon@naver.com>
2021-01-29 15:23:48 +0900

    html에 링크된 url 목록 추출

---
+++ showlink.js
@@ -0,0 +1,15 @@
+var client=require('cheerio-httpcli');
+
+var url="http://jpub.tistory.com";
+var param={};
+
+client.fetch(url, param, function(err, $, res){
+    if (err){ console.log("error"); return;}
+
+    //링크를 추출하여 표시
+    $("a").each(function(idx){
+        var text=$(this).text();
+        var href=$(this).attr('href');
+        console.log(text+":"+href);
+    });
+});
